e606c550caa89f62ba82f2971514567d00a11a57
Dale Emery <dale@dhemery.com>
2026-09-25 17:38:34 -0700

    Flesh out Citation

--- web/citation.go
+++ web/citation.go
@@ -1,6 +1,10 @@
 package web
 
-import "github.com/genealogix/glx/go-glx"
+import (
+	"strings"
+
+	"github.com/genealogix/glx/go-glx"
+)
 
 // Citation represents a GLX citation entity.
 type Citation struct {
@@ -8,12 +12,6 @@ type Citation struct {
 	g          *glx.Citation
 	ID         string
 	properties map[string]Property
-}
-
-// String returns a string representation of c formed by concatenating its
-// locator (if it has one) onto its source's Title.
-func (c *Citation) String() string {
-	return "Citation " + c.ID
 }
 
 func newCitation(id string, gc *glx.Citation, a *Archive) *Citation {
@@ -24,10 +22,40 @@ func newCitation(id string, gc *glx.Citation, a *Archive) *Citation {
 	}
 }
 
+func (c *Citation) Media() []*Media {
+	return c.a.mediaWithIDs(c.g.Media)
+}
+
+func (c *Citation) Notes() glx.NoteList {
+	return c.g.Notes
+}
+
 // Properties returns a map of c's properties indexed by name.
 func (c *Citation) Properties() map[string]Property {
 	if c.properties == nil {
 		c.properties = newProperties(c.g.Properties, c.a.g.CitationProperties, c.a)
 	}
 	return c.properties
+}
+
+func (c *Citation) Repository() *Repository {
+	return c.a.Repositories[c.g.RepositoryID]
+}
+
+func (c *Citation) Source() *Source {
+	return c.a.Sources[c.g.SourceID]
+}
+
+// String returns a string representation of c formed by concatenating its
+// locator (if it has one) onto its source's Title.
+func (c *Citation) String() string {
+	parts := []string{
+		c.Source().Title(),
+	}
+
+	if locator, ok := c.Properties()["locator"]; ok {
+		parts = append(parts, locator.String())
+	}
+
+	return strings.Join(parts, ", ")
 }
